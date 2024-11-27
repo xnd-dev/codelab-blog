0bff48a3f12eb2dd823988cb0ec7d8b1b343e229
Alexandre Barbosa <xnd.dev@gmail.com>
2024-11-27 20:55:46 -0300

    feat: state management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,24 @@ import Heart2 from '../../public/assets/heart-active.svg'
 import styles from "./page.module.css";
 import { useState } from "react";
 
-const posts = [
+const initialPosts = [
   {
+    id: 1,
     date: "17 de ago, 2024",
     title: "O que é linguagem de programação? Conheça as principais:",
     subtitle: "Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.", 
     isLiked: false,
   },
-  { date: "17 de ago, 2024",
+  { 
+    id: 2,
+    date: "17 de ago, 2024",
     title:"GitHub agora permite fazer login sem precisar de senha.",
     subtitle:"O GitHub anunciou nesta quarta-feira (12) o acesso a partir das passkeys, método de autenticação sem senhas. A novidade está disponível em uma versão beta pública e pode substituir a autenticação em dois fatores.", 
     isLiked: false,
   },
-  { date: "17 de ago, 2024",
+  { 
+    id:3,
+    date: "17 de ago, 2024",
     title:"Por que os hiperlinks são azuis em sua maioria?",
     subtitle:"Quem navega na internet, certamente já percebeu que ela conta com diversos recursos para tornar a nossa vida mais fácil. Entre essas opções podemos mencionar os hiperlinks – uma palavra ou termo clicável que direciona o leitor.", 
     isLiked: false,
@@ -25,10 +30,14 @@ const posts = [
 ]
 
 export default function Home() {
-  const [isFilled, setIsFilled] = useState(false)
+  const [posts, setPosts] = useState(initialPosts)
 
-  const toggleIcon = () => {
-    setIsFilled(!isFilled);
+  const toggleLike = (id:number) => {
+    setPosts((prevPosts) =>
+      prevPosts.map((post) => 
+        post.id === id ? { ...post, isLiked: !post.isLiked } : post
+      )
+    );
   };
 
   return (
@@ -43,56 +52,23 @@ export default function Home() {
       </header>
 
       <main className={styles.main}>
-        <div className={styles.box}>
-          <div className={styles.date}>
-            17 de ago, 2024 
-            <Image
-              src={isFilled ? Heart2 : Heart1} 
-              alt="curtir"
-              onClick={toggleIcon}
-            />
-          </div>
-
-          <div className={styles.content}>
-            <h2 className={styles.title}>O que é linguagem de programação? Conheça as principais:</h2>
-            <h3 className={styles.subtitle}>Uma das mais populares vertentes da tecnologia da informação,
-               a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.</h3>
-          </div>
-        </div>
-
-        <div className={styles.box}>
-          <div className={styles.date}>
-            17 de ago, 2024 
-            <Image 
-              src={isFilled ? Heart2 : Heart1} 
-              alt="curtir"
-              onClick={toggleIcon} 
+        {posts.map((post)=> (
+          <div key={post.id} className={styles.box}>
+            <div className={styles.date}>
+              {post.date}
+              <Image
+                src={post.isLiked ? Heart2 : Heart1} 
+                alt={post.isLiked ? "Descurtir" : "Curtir"}
+                onClick={() => toggleLike(post.id)}
               />
+            </div>
+            <div className={styles.content}>
+              <h2 className={styles.title}>{post.title}</h2>
+              <h3 className={styles.subtitle}>{post.subtitle}</h3>
+            </div>
           </div>
-          <div className={styles.content}>
-            <h2 className={styles.title}>GitHub agora permite fazer login sem precisar de senha.</h2>
-            <h3 className={styles.subtitle}>O GitHub anunciou nesta quarta-feira (12) o acesso a partir das passkeys, método de autenticação sem senhas.
-               A novidade está disponível em uma versão beta pública e pode substituir a autenticação em dois fatores.</h3>
-          </div>
-        </div>
-
-        <div className={styles.box}>
-          <div className={styles.date}>
-            17 de ago, 2024 
-            <Image 
-              src={isFilled ? Heart2 : Heart1} 
-              alt="curtir"
-              onClick={toggleIcon}
-              />
-          </div>
-          <div className={styles.content}>
-            <h2 className={styles.title}>Por que os hiperlinks são azuis em sua maioria?</h2>
-            <h3 className={styles.subtitle}>Quem navega na internet, certamente já percebeu que ela conta com diversos recursos para tornar a nossa vida mais fácil.
-               Entre essas opções podemos mencionar os hiperlinks – uma palavra ou termo clicável que direciona o leitor.</h3>
-          </div>
-        </div>
+                ))}
       </main>
-
     </div>
   );
 }
